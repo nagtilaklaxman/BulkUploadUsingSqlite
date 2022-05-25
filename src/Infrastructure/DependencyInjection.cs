f06c5cd3241583fcfdc610bfe000c0cf2e69d84b
Laxman Nagtilak <laxman@displaysweet.com>
2022-05-26 00:34:05 +1000

    Add unittest project for core

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Infrastructure.FileHelper;
+using Infrastructure.Interfaces.FileHelper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(ICsvHelper<>), typeof(CsvHelper<>));
+            services.AddScoped(typeof(IExcelHelper<>), typeof(ExcelHelper<>));
+
+
+            services.AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSQLite()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=test.db"))
+                // Define the assembly containing the migrations
+                //.ScanIn(typeof(AddLogTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole());
+            return services;
+        }
+    }
+}
+
---
+++ tests/Core.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
